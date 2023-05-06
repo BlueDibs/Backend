67eafec31bb454006965faa438d942649869b3a6
amiralam0 <sharpprogrammer2018@gmail.com>
2023-05-06 19:46:29 +0530

    pos upload endpoints

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,24 +1,38 @@
-import { Controller, Post, Req, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, Param, Post, Req, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { PrismaService } from "src/Prisma.Service";
 import * as bcrypt from 'bcrypt'
 import { FileInterceptor } from "@nestjs/platform-express";
 import { app, bucket } from "src/firebase";
+import { SaveOptions } from "@google-cloud/storage";
 
 @Controller('post')
 export class PostController {
     constructor(private pService: PrismaService) { }
 
-    @Post()
+    @Post(':id')
+    @HttpCode(HttpStatus.OK)
     @UseInterceptors(FileInterceptor('file'))
-    async addPost(@Req() req, @UploadedFile('file') file: Express.Multer.File) {
+    async addPost(@Param('id') id, @UploadedFile('file') file: Express.Multer.File) {
+        console.log(file)
         if (!file) return
         const filename = file.originalname.split('.')[0] + Date.now() + '.' + file.originalname.split('.').pop()
         const fileRef = bucket.file(filename)
-        await fileRef.save(file.buffer, { contentType: file.mimetype })
-        this.pService.post.create({
+        await fileRef.save(file.buffer, { contentType: file.mimetype, cacheControl: 'public, max-age=31536000' } as SaveOptions)
+        const res = await this.pService.post.create({
             data: {
                 path: filename,
-                userId: req.user.id
+                userId: id
+            }
+        })
+
+        console.log(res)
+    }
+
+    @Get(':id')
+    getPostRelatedToUser(@Param('id') id) {
+        return this.pService.post.findMany({
+            where: {
+                userId: id
             }
         })
     }
