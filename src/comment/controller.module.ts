041dbffa76a00218b7246daf6669ff0f593ecd4a
MohdAhmad1 <dexk2015@gmail.com>
2023-07-08 18:21:11 +0530

    fixed comments not working

--- src/comment/controller.module.ts
+++ src/comment/controller.module.ts
@@ -1,56 +1,64 @@
-import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
-import { PrismaService } from "src/Prisma.Service";
-import { AddCommentDTO } from "./comment.DTO";
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { PrismaService } from 'src/Prisma.Service';
+import { AddCommentDTO } from './comment.DTO';
 
 @Controller('comment')
 export class CommentController {
-    constructor(private readonly pService: PrismaService) { }
+  constructor(private readonly pService: PrismaService) {}
 
-    @Get(':postId')
-    getPostIdComments(@Param('postId') postId) {
-        return this.pService.comment.findMany({
-            where: {
-                postId: postId
+  @Get(':postId')
+  getPostIdComments(@Param('postId') postId) {
+    return this.pService.comment.findMany({
+      where: {
+        postId: postId,
+      },
+      include: {
+        User: {
+          select: {
+            username: true,
+            avatarPath: true,
+            id: true,
+          },
+        },
+      },
+    });
+  }
+
+  @Post(':postId')
+  addACommentToAPost(
+    @Param('postId') postId,
+    @Req() req,
+    @Body() body: AddCommentDTO,
+  ) {
+    try {
+      return this.pService.comment.create({
+        data: {
+          ...body,
+          created: new Date().toISOString(),
+          User: {
+            connect: {
+              firebaseId: req.user.user_id,
             },
-            include: {
-                User: {
-                    select: {
-                        username: true,
-                        avatarPath: true,
-                        id: true
-                    }
-                }
-            }
-        })
-    }
-
-    @Post(':postId')
-    addACommentToAPost(@Param('postId') postId, @Req() req, @Body() body: AddCommentDTO) {
-        return this.pService.comment.create({
-            data: {
-                ...body,
-                created: new Date().toISOString(),
-                User: {
-                    connect: {
-                        firebaseId: req.user.user_id
-                    }
-                },
-                Post: {
-                    connect: {
-                        id: postId
-                    }
-                }
+          },
+          Post: {
+            connect: {
+              id: postId,
             },
-            include: {
-                User: {
-                    select: {
-                        username: true,
-                        avatarPath: true,
-                        id: true
-
-                    }
-                }
-            }
-        })
+          },
+        },
+        include: {
+          User: {
+            select: {
+              username: true,
+              avatarPath: true,
+              id: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
 }
