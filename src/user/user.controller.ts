2428a28e1eb8ad1b893b3a13e05dc1692238a30b
Amir Alam <sharpprogrammer2018@gmail.com>
2023-07-10 11:18:40 -0700

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -34,14 +34,14 @@ export class UserController {
   constructor(
     private readonly pService: PrismaService,
     private readonly holdingService: HoldingService,
-    private readonly userService: UserService,
+    private readonly userService: UserService
   ) {}
 
   @Get('wallet')
   async getUserFinancialInfo(@Req() req) {
     // get holdings
     const holdings = await this.holdingService.getUserHoldings(
-      req.user.user_id,
+      req.user.user_id
     );
 
     // get user profile for calculating stats
@@ -170,7 +170,7 @@ export class UserController {
       .flatMap((item) => item.Posts)
       .sort(
         (a, b) =>
-          new Date(b.created).getSeconds() - new Date(a.created).getSeconds(),
+          new Date(b.created).getSeconds() - new Date(a.created).getSeconds()
       );
 
     return posts;
@@ -253,7 +253,7 @@ export class UserController {
         if (typeof prev == 'number') return prev + cur.amount;
         else return prev.amount + cur.amount;
       },
-      0,
+      0
     ) as number;
 
     if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
@@ -328,7 +328,7 @@ export class UserController {
   async sellOwnEquity(@Req() req, @Body() body: SellOwnEquity) {
     await this.userService.sellPlatformEquity(
       req.user.user_id,
-      body.percentage,
+      body.percentage
     );
 
     return 'Sold';
@@ -339,7 +339,7 @@ export class UserController {
   @UseInterceptors(FileInterceptor('avatar'))
   async updateUser(
     @Req() req,
-    @UploadedFile('file') avatar: Express.Multer.File,
+    @UploadedFile('file') avatar: Express.Multer.File
   ) {
     const body = updateUserSchema.parse(req.body);
 
@@ -372,7 +372,7 @@ export class UserController {
       if (err.code == 'P2002' && err.meta.target.includes['username']) {
         throw new HttpException(
           'username already exsists',
-          HttpStatus.CONFLICT,
+          HttpStatus.CONFLICT
         );
       }
     }
@@ -385,7 +385,7 @@ export class UserController {
         where: {
           NOT: {
             followers: {
-              some: { id: req.user.id },
+              some: { firebaseId: req.user.id },
             },
           },
         },
